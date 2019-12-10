2db764fa272ac256f6715d02d264c188caf39257
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-10 18:51:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(counter)
+ counter = 10
+
+while counter > 0
+  puts "#{counter} SECOND(S)!"
+  counter -= 1
+end
+  return "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep
+  counter = 0
+until counter == 5
+  puts "The current number is less than 5."
+  counter += 1
+end
+  
+  
+  
 end
