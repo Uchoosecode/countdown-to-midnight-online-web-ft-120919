c226eb61dfbb2f6fec872b3d506b1bf6eada5ec3
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-10 19:00:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,17 +1,19 @@
 #write your code here
 
 def countdown(counter)
- counter = 10
+  counter = 10
 
-while counter > 0
-  puts "#{counter} SECOND(S)!"
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
   counter -= 1
-end
-  return "HAPPY NEW YEAR!"
+  end
+      return "HAPPY NEW YEAR!"
 end
 
 
 def countdown_with_sleep(count)
+  count = 5 
+  
 
 sleep 5.seconds
 end
